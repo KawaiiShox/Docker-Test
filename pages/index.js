f9f46465f84fdfa47f707fae5903051dcd81da4b
Jonas Weinhardt <jonas.weinhardt@kup-hn.de>
2022-03-03 07:26:46 +0100

    Test2

--- pages/index.js
+++ pages/index.js
@@ -13,7 +13,7 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href='https://nextjs.org'>Next.js! Test Https</a>
+          Welcome to <a href='https://nextjs.org'>Next.js! Test123</a>
         </h1>
 
         <p className={styles.description}>
